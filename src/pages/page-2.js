c793e37a0596eb5f357e8ceefd649e60614045c4
me5uts <95043731+me5uts@users.noreply.github.com>
2022-05-23 14:26:05 +0200

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,6 +10,28 @@ const SecondPage = () => (
     <h1>Movie page.</h1>
     <p>A mafia movie.</p>
     <Link to="/">Go back to the homepage</Link>
+  
+  const links = [
+  {
+    text: "365 days ",
+    url: "https://bamfilmizle.com/365-gun-izle-2064.html",
+    description:
+      "A mafia movie.",
+  },
+  {
+    text: "Testing clang.",
+    url: "https://clang.llvm.org/",
+    description:
+      "The Clang project provides a language front-end and tooling infrastructure for languages in the C language family (C, C++, Objective C/C++, OpenCL, CUDA, and RenderScript) for the LLVM project.",        
+  },
+  {
+    text: "Ab init page",
+    url: "https://www.abinit.org/",
+    description:
+      "A software suite to calculate the optical, mechanical, vibrational, and other observable properties of materials.",
+  },
+ 
+]
   </Layout>
 )
 
